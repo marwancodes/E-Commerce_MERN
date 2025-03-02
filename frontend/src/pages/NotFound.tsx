130dc9b1931d64b42c741dedf62d96636fc1694d
marwancoder <marwane.ouarradi@gmail.com>
2025-03-02 16:03:17 +0000

    Enhance 404 Page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,18 +1,31 @@
-import { Link } from "react-router-dom"
+import { Box, Container, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 
 
 const NotFound = () => {
+
+    const navigate = useNavigate();
+
+    setTimeout(() => {
+        navigate('/');
+    }, 3000);
+
   return (
-    <div>
-        <div className="flex flex-col items-center justify-center h-screen text-center">
-      <h1 className="text-4xl font-bold text-red-600">404 - Page Not Found</h1>
-      <p className="text-gray-500 mt-2">Sorry, the page you are looking for does not exist.</p>
-      <Link to="/" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
-        Go Back Home
-      </Link>
-    </div>
-    </div>
+    <Container sx={{ mt: 4 }} maxWidth="md">
+        <Box display="flex" justifyContent="center">
+            <img src="https://freight.cargo.site/w/1200/i/33bf886cb6a718eb75fb9b88e943a718a473500a7bc6374c6a8e7dadb6641de8/404-SHOPORTFOLIO-1-transp.png" width={250} alt="404" />
+        </Box>
+        <Box display="flex" flexDirection="column" alignItems="center">
+            <Typography variant="h3" component="h1" gutterBottom>
+                Page Not Found
+            </Typography>
+            <Typography variant="h5" component="h2" gutterBottom>
+                We can't find the page you are looking for.
+            </Typography>
+        </Box>
+
+    </Container>
   )
 }
 
